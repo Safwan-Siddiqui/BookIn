e387303bf71e0d57edab298aaecd9a4e82b8b588
Safwan <safsiddiqui04@gmail.com>
2025-06-07 01:50:43 +0530

    Add Project Files

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema ({ //passport-local-mongoose automatically define, create, saves password and username
+    email: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', userSchema);
